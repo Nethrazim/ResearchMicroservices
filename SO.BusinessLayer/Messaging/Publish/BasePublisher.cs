9ab3193c79d904569283e31ca68508df6a272281
Nethrazim <vlad.branzei@gmail.com>
2022-04-23 11:41:14 +0300

    + Added MassTransit Publish/Consume Events

---
+++ SO.BusinessLayer/Messaging/Publish/BasePublisher.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SO.BusinessLayer.Messaging.Publish
+{
+    public class BasePublisher
+    {
+        private readonly IPublishEndpoint PublishEndpoint;
+        public BasePublisher(IPublishEndpoint publishEndpoint)
+        {
+            PublishEndpoint = publishEndpoint;
+        }
+
+        public void Publish<T>(T entity) where T : class
+        {
+            PublishEndpoint.Publish<T>(entity);
+        }
+    }
+}
---
+++ SO.BusinessLayer/Messaging/Publish/IUserPublisher.cs
@@ -0,0 +1,12 @@
+﻿using SO.BusinessLayer.Messaging.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO.BusinessLayer.Messaging.Publish
+{
+    public interface IUserPublisher : IBasePublisher<IUserChangedEvent>
+    {
+
+    }
+}
