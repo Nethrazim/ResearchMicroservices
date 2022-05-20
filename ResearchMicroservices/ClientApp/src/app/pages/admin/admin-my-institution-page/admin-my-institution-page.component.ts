34c47c4873de849a460f54d4d73b45cfa66c4471
Nethrazim <vlad.branzei@gmail.com>
2022-05-20 21:10:14 +0300

    + Changing the application Theme

--- ResearchMicroservices/ClientApp/src/app/pages/admin/admin-my-institution-page/admin-my-institution-page.component.ts
+++ ResearchMicroservices/ClientApp/src/app/pages/admin/admin-my-institution-page/admin-my-institution-page.component.ts
@@ -20,6 +20,10 @@ export class AdminMyInstitutionPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  Back() {
+    this.ResetDisplayOptions();
+    this.displayMenu = true;
+  }
   ResetDisplayOptions() {
     this.displayManageInstitution = false;
     this.displayManageContacts = false;
